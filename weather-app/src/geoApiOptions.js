95ba09c195cc55a171c34b1ac89b311f973f2a7e
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-16 12:09:48 -0500

    connected geo api for auto complete

---
+++ weather-app/src/geoApiOptions.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "54e00a8135msh5e8f5cf79ad91ccp1029f0jsn0a41a93811be",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
