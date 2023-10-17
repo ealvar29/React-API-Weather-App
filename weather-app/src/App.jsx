3dc37a8dcd78e9218070bf071154afba39692d3e
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-17 17:26:41 -0500

    adding weather api key

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -4,12 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./geoApiOptions";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, long] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}`
+    );
   };
   return (
     <div className="container">
--- weather-app/src/geoApiOptions.js
+++ weather-app/src/geoApiOptions.js
@@ -8,3 +8,7 @@ export const geoApiOptions = {
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "a3197354d83932a000ff2e8b0c4b50a4";
